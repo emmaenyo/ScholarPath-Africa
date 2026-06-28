33450f0b2dc07964d467447efc5344c9393e0bd2
Emmanuel Enyo Agbeko <142950287+emmaenyo@users.noreply.github.com>
2026-06-27 21:54:05 -0400

    Add AdSense, GA, and verification to all pages via _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,16 +5,32 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8236303795073111"
+          crossOrigin="anonymous"
+        />
+
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E6PD3KTZ69" />
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-E6PD3KTZ69');
+        `}} />
+
+        {/* Google Search Console */}
+        <meta name="google-site-verification" content="mQ9MKDhYFG4Ny86Uu9fsZaLQ5gRRNzmhMHSYT4txG4Y" />
+
         {/* Favicon */}
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/favicon.svg" />
-
-        {/* Site identity for social sharing */}
-        <meta property="og:image" content="/og-image.svg" />
         <meta name="theme-color" content="#16a34a" />
 
-        {/* Google Fonts */}
+        {/* Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=DM+Sans:wght@400;500;600&display=swap" rel="stylesheet" />
